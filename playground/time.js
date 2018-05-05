6d933217f205f8b612e97e83c5ae5f40224bc159
Hein Dauven <heindauven@gmail.com>
2018-05-06 00:30:57 +0200

    Try momentjs with an existing value.

--- playground/time.js
+++ playground/time.js
@@ -7,3 +7,10 @@ var date = moment();
 date.add(100, 'years').subtract(9, 'months');
 console.log(date.format('MMM Do, YYYY'));
 console.log(date.format('h:mm a'));
+
+var createdAt = (123456);
+var oldDate = moment(createdAt);
+console.log(oldDate.format('h:mm a'));
+
+var someTimestamp = moment().valueOf();
+console.log(someTimestamp);
